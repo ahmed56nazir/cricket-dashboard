b9c3ebfa199d472981704cfd7bcee593ddd21f1c
Ahmed Nazir <ahmed56nazir@gmail.com>
2026-06-08 10:50:33 +0500

    Update team controller after testing

--- controllers/team.js
+++ controllers/team.js
@@ -1,12 +1,38 @@
 const Team = require("../models/team")
+const Player = require("../models/player")
 
 async function getTeams(req, res) {
     try {
         const teams = await Team.find()
-        res.render("teams", {teams})
-    } catch (err) {
-        res.json({error: err.message})
+        const players = await Player.find()
+
+        const teamsWithPlayerIds = teams.map(team => {
+            const updatedPlayers = team.players.map(teamPlayer => {
+                const found = players.find(p =>
+                    p.name.toLowerCase() === teamPlayer.name.toLowerCase()
+                )
+                return {
+                    name: teamPlayer.name,
+                    role: teamPlayer.role,
+                    playerId: found ? found.playerId : null
+                }
+            })
+            return {
+                ...team.toObject(),
+                players: updatedPlayers
+            }
+        })
+
+        if(teamsWithPlayerIds) {
+            res.render("teams", { teams: teamsWithPlayerIds })
+        } else {
+            res.end("Error getting teams..")
+        }
+    } catch(err) {
+        res.json({ error: err.message })
     }
 }
 
-module.exports = {getTeams}
+module.exports = {
+    getTeams
+}
